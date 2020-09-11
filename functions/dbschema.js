763db4e168b9266e2964f7d4e5c18d39ad226cad
Jonathan Cobb <jmc846@scarletmail.rutgers.edu>
2020-09-11 11:18:52 -0400

    changes

---
+++ functions/dbschema.js
@@ -0,0 +1,16 @@
+let db = {
+ screams: [
+   {
+     userHandle: 'user',
+     body: 'this is scream body',
+     createdAt: "2020-09-11T02:04:04.485Z",
+     likeCount: 5,
+     commentCount:2
+
+   }
+
+
+ ]
+
+
+};
